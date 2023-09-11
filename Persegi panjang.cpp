2765583a265ac41522cb2351087199d4d280c0c7
reradwiput <144641062+reradwiput@users.noreply.github.com>
2023-09-11 09:50:35 +0700

    Create Persegi panjang.cpp

---
+++ Persegi panjang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+int p,l;
+cout<<"PEMROGRAMAN MENGHITUNG LUAS PERSEGI PANJANG \n";
+cout<<"masukan nilai panjang: ";
+cin>>p;
+cout<<"masukan nilai lebar: ";
+cin>>l;
+l=p*l;
+cout<<"hasilnya luas persegi panjang: "<<l<< endl;
+return 0;
+}
